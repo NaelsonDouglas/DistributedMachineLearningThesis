ec36999eba3bc878d36bf46c4e8271195d98c3a7
Andre Lage <prof.alage@gmail.com>
2018-09-21 11:33:24 -0300

    added the prototype flag to dockerrun function
    * now we can mount a common data shared space for all deployed
    containers (to enable shared memory access through host FS)

--- src/docker/DockerBackend.jl
+++ src/docker/DockerBackend.jl
@@ -35,11 +35,11 @@ end
 function dockerrun(;img="dmlt", params="-tid", nofcpus=1, memlimit=2048, prototype::Bool=false)
 	#TODO param --cpuset-cpus : CPUs in which to allow execution (0-3, 0,1)
 	memlimit = memlimit * 10^6 # converting mem to MB
-	cmd = Cmd()
+	cmd = ``
 	if prototype
 		cmd = Cmd(`docker run $params
-			-v ~/results-111111111:/DistributedMachineLearningThesis/src/results
-			--cpus $nofcpus -m $memlimit $img`) #TODO Dates.now(...)
+			-v /tmp/results-$(Dates.format(Dates.now(),"yyyy-mm-dd-HH.MM.SS")):/DistributedMachineLearningThesis/src/results
+			--cpus $nofcpus -m $memlimit $img`)
 	else
 		cmd = Cmd(`docker run $params --cpus $nofcpus -m $memlimit $img`)
 	end
@@ -237,6 +237,9 @@ function test_docker_backend()
 
 	println("\n\n== TEST > removing an unexistent container, should print an Error")
 	dockerrm("a")
+
+	# println("\n\n== TEST > PTOTORYPE parameter of dockerrun function")
+	# cid = dockerrun(prototype=true)
 end
 
 #
