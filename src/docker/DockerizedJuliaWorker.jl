ee9a7ee1940f005e534b7dfbec115a3b9f711635
Andre Lage <prof.alage@gmail.com>
2018-09-25 14:13:06 -0300

    `ad2` worked in GranColosso, renamed it to `adddockerworker`

--- src/docker/DockerizedJuliaWorker.jl
+++ src/docker/DockerizedJuliaWorker.jl
@@ -26,53 +26,6 @@ If successful, return the list of the deployed Worker(s), otherwise
 `exit(1)` from Julia."
 function adddockerworkers(nofworkers::Int;_img="dmlt", _params="-tid",
 							_nofcpus=1, _memlimit=2048, _prototype::Bool=false)
-	#nofworkers=1
-	#img="dmlt"; params="-tid"; nofcpus=1; memlimit=2000
-	ssh_key=homedir()*"/.ssh/id_rsa"
-	juliabin = "/root/julia/bin/julia"
-	pids = Vector()
-
-	info("Deploying Docker $nofworkers container(s) and initialize their SSH daemon...")
-	for n in 1:nofworkers
-		# 1. deploy a container
-		cid = dockerrun(img=_img,params=_params,nofcpus=_nofcpus,
-						memlimit=_memlimit,prototype=_prototype)
-		# 2. initiate SSHD on the deployed container
-		if ! sshup(cid)
-			error("Could NOT init SSH at container $cid. Exiting Julia...")
-			exit(1)
-		end
-
-		# 3. get container's IP and create a Worker by using it
-		info("Getting containers' IP address(es)...")
-		ip = get_containerip(cid)[1]
-		info("Containers' IP address is $ip")
-
-		info("Creating $n-th Worker(s) through SSH...")
-		pid = -1
-		try
-			pid = addprocs(
-				[ip],
-				sshflags=`-i $ssh_key -o "StrictHostKeyChecking no"`,
-				exename="$juliabin")
-		catch
-			error("Could NOT create $n-th Worker! \n
-				Worker(s)' IP addresses: $ip \n
-				Exiting Julia...")
-			exit(1)
-		end
-		info("Deployed workers ID is: $pid")
-
-		push!(pids, pid)
-		new_maped_pid = Dict(pid[1] => cid)
-		merge!(cids_pids_map, new_maped_pid)
-	end # loop
-
-	return pids
-end
-
-function ad2(nofworkers::Int;_img="dmlt", _params="-tid",
-							_nofcpus=1, _memlimit=2048, _prototype::Bool=false)
 	ssh_key=homedir()*"/.ssh/id_rsa"
 	juliabin = "/root/julia/bin/julia"
 	pids = Vector()
