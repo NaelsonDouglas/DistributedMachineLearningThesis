cc74645863d0501ab5be5039b4fa0def7b7988a5
Andre Lage <prof.alage@gmail.com>
2018-09-25 13:00:51 -0300

    new version of adddockerworker named ad2 to solve Issue #11

--- src/docker/DockerizedJuliaWorker.jl
+++ src/docker/DockerizedJuliaWorker.jl
@@ -34,13 +34,16 @@ function adddockerworkers(nofworkers::Int;_img="dmlt", _params="-tid",
 
 	info("Deploying Docker $nofworkers container(s) and initialize their SSH daemon...")
 	for n in 1:nofworkers
+		# 1. deploy a container
 		cid = dockerrun(img=_img,params=_params,nofcpus=_nofcpus,
 						memlimit=_memlimit,prototype=_prototype)
+		# 2. initiate SSHD on the deployed container
 		if ! sshup(cid)
 			error("Could NOT init SSH at container $cid. Exiting Julia...")
 			exit(1)
 		end
 
+		# 3. get container's IP and create a Worker by using it
 		info("Getting containers' IP address(es)...")
 		ip = get_containerip(cid)[1]
 		info("Containers' IP address is $ip")
@@ -64,6 +67,70 @@ function adddockerworkers(nofworkers::Int;_img="dmlt", _params="-tid",
 		new_maped_pid = Dict(pid[1] => cid)
 		merge!(cids_pids_map, new_maped_pid)
 	end # loop
+
+	return pids
+end
+
+function ad2(nofworkers::Int;_img="dmlt", _params="-tid",
+							_nofcpus=1, _memlimit=2048, _prototype::Bool=false)
+	ssh_key=homedir()*"/.ssh/id_rsa"
+	juliabin = "/root/julia/bin/julia"
+	pids = Vector()
+	ips = Vector()
+	pids_cids_map = Dict()
+
+	info("Deploying Docker $nofworkers container(s) and initialize their SSH daemon...")
+	for n in 1:nofworkers
+		# 1. deploy a container
+		cid = dockerrun(img=_img,params=_params,nofcpus=_nofcpus,
+						memlimit=_memlimit,prototype=_prototype)
+		# 2. initiate SSHD on the deployed container
+		if ! sshup(cid)
+			error("Could NOT init SSH at container $cid. Exiting Julia...")
+			exit(1)
+		end
+		# 3. get container's IP and create a Worker by using it
+		info("Getting containers' IP address(es)...")
+		ip = get_containerip(cid)[1]
+		push!(ips,ip)
+		println("DEBUG: type of ip is $(typeof(ip))")
+		info("Containers' IP address is $ip")
+
+		# 4. adding cid to a temporary pid_cid Dict
+		new_cid = Dict(cid => -1)
+		merge!(pids_cids_map, new_cid)
+	end
+
+	info("Creating $nofworkers Worker(s) through SSH...")
+	try
+		pids = addprocs(
+			ips,
+			sshflags=`-i $ssh_key -o "StrictHostKeyChecking no"`,
+			exename="$juliabin")
+	catch
+		error("Could NOT create $nofworkers Worker(s)!\n
+			Worker(s)' IP addresses: $ips\nExiting Julia...")
+		exit(1)
+	end
+	info("Creating $nofworkers Worker(s) through SSH... DONE.
+			Workers' PIDs are \n$pids\n")
+
+	info("Getting Workers' container IDs to update the pids-cid Dict...")
+	for w in workers()
+		cid = -1
+		try
+			cid = remotecall_fetch(readstring,w,pipeline(`head -1 /proc/self/cgroup`,`cut -d/ -f3`))
+		catch
+			error("Could NOT get container ID by calling Worker $w\nExiting Julia...")
+			exit(1)
+		end
+		cid = chomp(cid)
+		pids_cids_map[cid] = w # update the k-v to the right value (CID => Worker ID)
+	end
+
+	# inverting and merging the local pids_cids_map to the global cids_pids_map Dict
+	pids_cids_map = Dict(zip(collect(values(pids_cids_map)),collect(keys(pids_cids_map))))
+	merge!(cids_pids_map, pids_cids_map)
 
 	return pids
 end
