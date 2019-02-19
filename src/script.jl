6415e9857394eac46c38ebfa0e38a7752a2b8f2f
naelsondouglas <naelson17@gmail.com>
2019-02-19 02:52:54 -0300

    a

--- src/script.jl
+++ src/script.jl
@@ -20,11 +20,15 @@ idx_num_neighboors = 1
 for idx_seeds in seeds
     
   start_time = Dates.format(Dates.now(),"yy-mm-dd-HH:MM:SS")
-  args =["4", data_size[1], functions[1], idx_seeds, num_neighboors[1], dim_functions[1],"summary"]     
+  args =["4", data_size[1], functions[1], idx_seeds, num_neighboors[1], dim_fun[1],"summary"]     
   cids_pids_map = Dict()
   folder = execute_experiment(args)
+  mv(folder,folder*"_"*start_time)
+  folder = folder,folder*"_"*start_time
   
   output_folder = "./results/"*join([args[1], args[2], args[3], idx_seeds, args[6], args[7]], "_")
+  output_folder = output_folder*"_"*start_time
+
   mkpath(output_folder)
   run(`mv $folder $output_folder`)  
 
