49fdac2967e7a768ec2dfd6e948ff9af5d1de89a
naelsondouglas <naelson17@gmail.com>
2019-02-19 05:27:43 -0300

    Changed the order the parameters change on the experiment

--- src/script.jl
+++ src/script.jl
@@ -17,25 +17,25 @@ idx_dim_functions = idx_functions
 idx_num_neighboors = 1
 =#
 
-  for idx_data_size in data_size
-    for idx_num_nodes in num_nodes
-      for idx_seeds in seeds
-          #for idx_functions in functions
-            for idx_num_neighboors in num_neighboors
-              
-              start_time = Dates.format(Dates.now(),"yy-mm-dd-HH:MM:SS")
-              args =[idx_num_nodes, idx_data_size, "f1", idx_seeds, idx_num_neighboors, "2","summary"]     
-              cids_pids_map = Dict()
+for idx_num_neighboors in num_neighboors  
+    for idx_seeds in seeds
+        #for idx_functions in functions
+          for idx_num_nodes in num_nodes
+            for idx_data_size in data_size
+            
+            start_time = Dates.format(Dates.now(),"yy-mm-dd-HH:MM:SS")
+            args =[idx_num_nodes, idx_data_size, "f1", idx_seeds, idx_num_neighboors, "2","summary"]     
+            cids_pids_map = Dict()
 
-              folder = execute_experiment(args)
-              mv(folder,folder*"_"*start_time)
-              rmalldockerworkers()
+            folder = execute_experiment(args)
+            mv(folder,folder*"_"*start_time)
+            rmalldockerworkers()
+            end #data_size
+          end #num_nodes        
+        #end #functions
+    end #seeds  
+end #idx_num_neighboors
 
-            end #idx_num_neighboors
-          #end #functions
-      end #seeds
-    end #num_nodes
-  end #data_size
 
 
 
