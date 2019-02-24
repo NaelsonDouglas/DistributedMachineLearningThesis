d0e31b4ea538a2fdf58e296ef5c73dc74062d604
naelsondouglas <naelson17@gmail.com>
2019-02-24 15:19:35 -0300

    Fixed the script.sh

--- src/script.sh
+++ src/script.sh
@@ -1,6 +1,33 @@
-#!/usr/bin/env bash
+functions=(f1 f2 f4)
 
-for i in {1..10}
+declare -A dim_functions
+dim_functions=( ["f1"]="2" ["f2"]="3" ["f4"]="5")
+data_size=(1000 10000 100000)
+num_nodes=(8 16 20 24 28 32 36)
+seeds=(1111  2222 3333  4444  5555  6666  7777  8888  9999  1234)
+num_neighboors=(2 3)
+
+for repetitions in {1..10}
 do
-   julia ugly_script.jl 8 1000 f1 1111 2 2 summary
+		for  f in ${functions[@]}
+		do
+
+			for ds in ${data_size[@]}
+			do
+				for n_nodes in ${num_nodes[@]}
+				do
+					for s in ${seeds[@]}
+					do
+						for sds in ${seeds[@]}
+						do
+							for num_nei in ${num_neighboors[@]}
+							do							
+								echo $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]}								
+								JULIABIN ugly_script.jl $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]} summary
+							done
+						done
+					done
+				done
+			done
+		done   
 done
