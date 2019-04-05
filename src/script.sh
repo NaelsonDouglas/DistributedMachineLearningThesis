86204fefb8466fdee59d9a0d3f49e55b1f303b16
naelsondouglas <naelson17@gmail.com>
2019-04-05 04:57:24 +0000

    Added a few more results:w

--- src/script.sh
+++ src/script.sh
@@ -2,7 +2,7 @@ functions=(f1 f2 f4)
 
 declare -A dim_functions
 dim_functions=( ["f1"]="2" ["f2"]="3")
-data_size=(16000 64000)
+data_size=(64000)
 num_nodes=(16 32 64)
 seeds=(1111  2222 3333  4444  5555  6666  7777  8888  9999  1234)
 num_neighboors=(2 3)
