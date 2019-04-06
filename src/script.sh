31be07a19030415ee099c05a26e9ae07a00f0179
naelsondouglas <naelson17@gmail.com>
2019-04-05 23:23:49 -0300

    Added a failsafe to the experiment script

--- src/script.sh
+++ src/script.sh
@@ -4,7 +4,7 @@ declare -A dim_functions
 dim_functions=( ["f1"]="2" ["f2"]="3")
 data_size=(64000)
 num_nodes=(16 32 64)
-seeds=(1111  2222 3333  4444  5555  6666  7777  8888  9999  1234)
+seeds=(3333  4444  5555  6666  7777  8888  9999  1234)
 num_neighboors=(2)
 
 
@@ -23,7 +23,9 @@ num_neighboors=(2)
 								for repetitions	 in {1..10}
 								do						
 									echo $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]}								
-									/root/julia/bin/julia ugly_script.jl $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]} summary									
+									timeout 300 /root/julia/bin/julia ugly_script.jl $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]} summary
+									nodes=$(docker ps -q | grep -v $(hostname))
+									docker kill $nodes
 								done
 							done
 						done
