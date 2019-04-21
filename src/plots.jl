50c9e55d6335dfae92ef3f1d94fd8f185f110c3e
naelsondouglas <naelson17@gmail.com>
2019-04-21 11:26:16 -0300

    Some code cleaning

--- src/plots.jl
+++ src/plots.jl
@@ -2,33 +2,6 @@ using CSV
 
 results_folder="../partial_results/"
 
-#=
-"Just an auxiliar function which converts the kwards arguments to a formated query string"
-function kwargs_to_query(n_nodes=16,data_size=64000,func="f1",seed=1111,neighboors=2,dim=2)
-	return string(n_nodes)*"-"*string(data_size)*"-"*func*"-"*string(seed)*"-"*string(neighboors)*"-"*string(dim)
-end
-
-"Just an auxiliar function which converts the query to kwargs"
-function query_to_kwargs(query::String)
-	kwargs=collect(split(query,"-"))
-end
-
-
-"This function returns the availabe results for the typed parameters. It returns a list of directories"
-function filter_dirs(;_n_nodes=16,_data_size=64000,_func="f1",_seed=1111,_neighboors=2,_dim=2)
-	all_folders = readdir(results_folder)	
-	query = kwargs_to_query(_n_nodes,_data_size,_func,_seed,_neighboors,_dim)
-	results = []	
-	for f in all_folders
-		if contains(f,query)
-			push!(results,results_folder*f)
-		end
-	end	
-	return results
-end
-=#
-
-
 function check_token(tokens,variable_value,position::Int)
 	if string(variable_value) == "*" || string(tokens[position]) == string(variable_value)
 		return true
@@ -41,10 +14,6 @@ end
 function filter_dirs(;_n_nodes="*",_data_size="*",_func="*",_seed="*",_neighboors="*",_dim="*")
 	all_folders = readdir(results_folder)
 	positions = Dict{String,Int}("_n_nodes"=>1,"_data_size"=>2,"_func"=>3,"_seed"=>4,"_neighboors"=>5,"_dim"=>6)
-
-
-	
-
 	
 	results = []	
 	for f in all_folders
