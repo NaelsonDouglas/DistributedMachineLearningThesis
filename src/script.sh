368c48e47e505557f95550b740ae756dbfc1fef7
naelsondouglas <naelson17@gmail.com>
2019-05-08 14:40:24 +0000

    Added more results

--- src/script.sh
+++ src/script.sh
@@ -3,7 +3,7 @@ declare -A dim_functions
 dim_functions=( ["f1"]="2" ["f2"]="3")
 data_size=(16000)
 num_nodes=(16 32)
-seeds=(5555  6666  7777  8888  9999  1234)
+seeds=(7777  8888  9999  1234)
 num_neighboors=(4 2)
 		for  f in ${functions[@]}
 		do
@@ -20,7 +20,7 @@ num_neighboors=(4 2)
 								for repetitions	 in {1..10}
 								do						
 									echo $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]}								
-									timeout 1200 /root/julia/bin/julia ugly_script.jl $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]} summary
+									timeout 1800 /root/julia/bin/julia ugly_script.jl $n_nodes $ds $f $sds $num_nei ${dim_functions[$f]} summary
 									nodes=$(docker ps -q | grep -v $(hostname))
 									docker kill $nodes
 								done
